9af816c7ce41e702a8d24e77fe674aa42eceee1c
Jackie-89 <jaqueline.barcelo@hotmail.com>
2025-09-03 16:54:01 -0300

    tentando meu primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const message = "Oulááá povoss" // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+//variaveis a seguir
